09af1ea3e92412f8caaa71f74b688e099ca8a1d7
yaskazeko <yaskazeko@gmail.com>
2020-08-06 11:45:44 +0300

    add helper.py

--- table.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-import urllib.request
-
-request = urllib.request.urlopen('https://www.livescore.com/')
-html = request.read()
-
-html_pars = BeautifulSoup(html, 'html.parser')
-
-print(html_pars)
